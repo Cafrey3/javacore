4d3122228557af6d745838ba71b1015fbaea3cb8
Cafrey3 <tokar9980@gmail.com>
2023-03-04 17:13:15 +0200

    hw1

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -17,5 +17,11 @@ public class Main {
 
         Book bookInstance = new Book("Metro 2033", 383, authors, "post-apocalyptic");
         System.out.println(bookInstance);
+
+        Post postInstance = new Post(1, 1, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto");
+        System.out.println(postInstance);
+
+        Comment commentInstance = new Comment(1, 1, "id labore ex et quam laborum", "Eliseo@gardner.biz", "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium");
+        System.out.println(commentInstance);
     }
 }
